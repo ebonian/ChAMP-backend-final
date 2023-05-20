2f0a5bce5b099fda39117e2c0d0aecba6c9bb5e1
Ebonian <tanadon.santisan@gmail.com>
2023-05-20 23:50:22 +0700

    repo: add necessary package and create basic structure

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+const port = 3000;
+
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
