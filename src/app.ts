337efa6ac9c4b7a523dc93fdc6eaccd849f12f2f
Ebonian <tanadon.santisan@gmail.com>
2023-05-20 23:56:24 +0700

    chore: format file

--- src/app.ts
+++ src/app.ts
@@ -5,18 +5,18 @@ const app = express();
 const port = 3000;
 
 mongoose
-  .connect("mongodb://localhost:27017")
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect("mongodb://localhost:27017")
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
